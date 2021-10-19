1a593dcee0c9a38eb0b9eadb04b1a95167a5045d
Peter Molnar <petermolnar.hu@gmail.com>
2021-10-19 11:07:11 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Peter Molnar on 19/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
